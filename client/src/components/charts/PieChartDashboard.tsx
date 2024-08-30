c713ba5e00330ddafe16cdfd5572cb99a39ab1a7
reijjo <teemu.aitomeri@gmail.com>
2024-08-30 21:27:04 +0300

    PieChart to own component

---
+++ client/src/components/charts/PieChartDashboard.tsx
@@ -0,0 +1,82 @@
+import { PieChart, Pie, ResponsiveContainer, Label, Cell } from "recharts";
+import { Bet } from "../../utils/types";
+
+export type PieChartDashboardData = {
+  name: string;
+  value: number;
+  percent?: number;
+};
+
+type PieChartDashboardProps = {
+  myBets: Bet[];
+  hoverText: string | null;
+  onHover: (data: PieChartDashboardData | null) => void;
+};
+
+export const PieChartDashboard = ({
+  myBets,
+  hoverText,
+  onHover,
+}: PieChartDashboardProps) => {
+  const pieData: PieChartDashboardData[] = [
+    {
+      name: "Won",
+      value:
+        myBets.filter((b) => b.status === "Won").length +
+        myBets.filter((b) => b.status === "Half Won").length,
+    },
+    {
+      name: "Lost",
+      value:
+        myBets.filter((b) => b.status === "Lost").length +
+        myBets.filter((b) => b.status === "Half Lost").length,
+    },
+    {
+      name: "Void",
+      value:
+        myBets.filter((b) => b.status === "Void").length +
+        myBets.filter((b) => b.status === "Push").length,
+    },
+  ];
+
+  const colors = [
+    "var(--success-light)",
+    "var(--secondary-light)",
+    "var(--warning-medium)",
+  ];
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie
+          data={pieData}
+          cx="50%"
+          cy="50%"
+          innerRadius={60}
+          outerRadius={80}
+          fill="#8884d8"
+          paddingAngle={5}
+          dataKey="value"
+          stroke="none"
+          onMouseEnter={(entry) => onHover(entry)}
+          onMouseLeave={() => onHover(null)}
+        >
+          {pieData.map((_entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+
+          <Label
+            value={hoverText || 0} // Display hover text or default percent
+            position="center"
+            fill="#000"
+            style={{
+              fontSize: "1em",
+              fontWeight: "bold",
+              fill: "var(--text)",
+            }}
+          />
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
--- client/src/components/dashboard/dashboard-cards/win-percent-card/WinPercentCard.tsx
+++ client/src/components/dashboard/dashboard-cards/win-percent-card/WinPercentCard.tsx
@@ -1,62 +1,8 @@
 import "./WinPercentCard.css";
+import { PieChartDashboard } from "../../../charts";
 import { useState } from "react";
-import { PieChart, Pie, ResponsiveContainer, Label, Cell } from "recharts";
 import { useAppSelector } from "../../../../store/hooks";
-
-type PieData = {
-  name: string;
-  value: number;
-  percent?: number;
-};
-
-type PieChartProps = {
-  data: PieData[];
-  colors: string[];
-  hoverText: string | null;
-  onHover: (data: PieData | null) => void;
-};
-
-const PieChartTest = ({ data, colors, hoverText, onHover }: PieChartProps) => {
-  const wonbets = data.filter((b) => b.name === "Won");
-  const wonper = wonbets[0].percent;
-
-  console.log("wonper", wonper);
-
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <PieChart>
-        <Pie
-          data={data}
-          cx="50%"
-          cy="50%"
-          innerRadius={60}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="value"
-          stroke="none"
-          onMouseEnter={(entry) => onHover(entry)}
-          onMouseLeave={() => onHover(null)}
-        >
-          {data.map((_entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-          ))}
-
-          <Label
-            value={hoverText || `${wonper}`} // Display hover text or default percent
-            position="center"
-            fill="#000"
-            style={{
-              fontSize: "1em",
-              fontWeight: "bold",
-              fill: "var(--text)",
-            }}
-          />
-        </Pie>
-      </PieChart>
-    </ResponsiveContainer>
-  );
-};
+import { PieChartDashboardData } from "../../../charts/PieChartDashboard";
 
 export const WinPercentCard = () => {
   const mybets = useAppSelector((state) => state.bets.allBets);
@@ -79,7 +25,7 @@ export const WinPercentCard = () => {
   // Initialize hoverData with the default "Won" percentage
   const [hoverData, setHoverData] = useState<string>(defaultHoverText);
 
-  const handleHover = (data: PieData | null) => {
+  const handleHover = (data: PieChartDashboardData | null) => {
     if (data) {
       setHoverData(`${data.name}: ${(data.percent! * 100).toFixed(0)}%`);
     } else {
@@ -87,40 +33,11 @@ export const WinPercentCard = () => {
     }
   };
 
-  const pieData: PieData[] = [
-    {
-      name: "Won",
-      value:
-        mybets.filter((b) => b.status === "Won").length +
-        mybets.filter((b) => b.status === "Half Won").length,
-      percent: wonPercentage,
-    },
-    {
-      name: "Lost",
-      value:
-        mybets.filter((b) => b.status === "Lost").length +
-        mybets.filter((b) => b.status === "Half Lost").length,
-    },
-    {
-      name: "Void",
-      value:
-        mybets.filter((b) => b.status === "Void").length +
-        mybets.filter((b) => b.status === "Push").length,
-    },
-  ];
-
-  const colors = [
-    "var(--success-light)",
-    "var(--secondary-light)",
-    "var(--warning-medium)",
-  ];
-
   return (
     <div className="dash-winpercent">
       <h5>Win %</h5>
-      <PieChartTest
-        data={pieData}
-        colors={colors}
+      <PieChartDashboard
+        myBets={mybets}
         hoverText={hoverData}
         onHover={handleHover}
       />
