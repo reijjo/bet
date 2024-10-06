718c2980e04d167b6554e86cca457b8169f3c50a
reijjo <teemu.aitomeri@gmail.com>
2024-10-06 14:27:47 +0300

    bar chart next

--- client/src/components/dashboard/dashboard-cards/win-percent-card/WinPercentCard.tsx
+++ client/src/components/dashboard/dashboard-cards/win-percent-card/WinPercentCard.tsx
@@ -1,34 +1,50 @@
 import "./WinPercentCard.css";
-import { PieChartDashboard } from "../../../charts";
-import { useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useAppSelector } from "../../../../store/hooks";
+import { PieChartDashboard } from "../../../charts";
 import { PieChartDashboardData } from "../../../charts/PieChartDashboard";
 
 export const WinPercentCard = () => {
+  const [hoverData, setHoverData] = useState<string>("Loading...");
+
   const mybets = useAppSelector((state) => state.bets.allBets);
 
-  const wonBetsCount =
-    mybets.filter((b) => b.status === "Won").length +
-    mybets.filter((b) => b.status === "Half Won").length;
+  useEffect(() => {
+    if (mybets.length > 0) {
+      const wonBetsCount =
+        mybets.filter((b) => b.status === "Won").length +
+        mybets.filter((b) => b.status === "Half Won").length;
 
-  console.log("wonBetsCount: ", wonBetsCount);
+      const totalBetsCount = mybets.filter(
+        (b) => b.status !== "Pending"
+      ).length;
 
-  const totalBetsCount = mybets.filter((b) => b.status !== "Pending").length;
+      const wonPercentage =
+        totalBetsCount > 0 ? wonBetsCount / totalBetsCount : 0;
 
-  console.log("totlaBetsCount: ", totalBetsCount);
+      const defaultHoverText = `Won: ${(wonPercentage * 100).toFixed(0)}%`;
 
-  // Calculate the default percentage for "Won" bets
-  const wonPercentage = wonBetsCount / totalBetsCount;
-
-  const defaultHoverText = `Won: ${(wonPercentage * 100).toFixed(0)}%`;
-
-  // Initialize hoverData with the default "Won" percentage
-  const [hoverData, setHoverData] = useState<string>(defaultHoverText);
+      setHoverData(defaultHoverText);
+    }
+  }, [mybets]);
 
   const handleHover = (data: PieChartDashboardData | null) => {
     if (data) {
       setHoverData(`${data.name}: ${(data.percent! * 100).toFixed(0)}%`);
     } else {
+      const wonBetsCount =
+        mybets.filter((b) => b.status === "Won").length +
+        mybets.filter((b) => b.status === "Half Won").length;
+
+      const totalBetsCount = mybets.filter(
+        (b) => b.status !== "Pending"
+      ).length;
+
+      const wonPercentage =
+        totalBetsCount > 0 ? wonBetsCount / totalBetsCount : 0;
+
+      const defaultHoverText = `Won: ${(wonPercentage * 100).toFixed(0)}%`;
+
       setHoverData(defaultHoverText);
     }
   };
@@ -36,11 +52,13 @@ export const WinPercentCard = () => {
   return (
     <div className="dash-winpercent">
       <h5>Win %</h5>
-      <PieChartDashboard
-        myBets={mybets}
-        hoverText={hoverData}
-        onHover={handleHover}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <PieChartDashboard
+          myBets={mybets}
+          hoverText={hoverData}
+          onHover={handleHover}
+        />
+      </Suspense>
     </div>
   );
 };
