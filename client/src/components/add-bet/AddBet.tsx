79ea4aa98273ebf9a451144dce3137504fc8a8c1
reijjo <teemu.aitomeri@gmail.com>
2024-12-06 12:20:55 +0200

    mybetslip started

--- client/src/components/add-bet/AddBet.tsx
+++ client/src/components/add-bet/AddBet.tsx
@@ -4,21 +4,19 @@ import { useState } from "react";
 
 import { Bet } from "../../utils/types";
 import { AddBetForm } from "../index";
-// import { MyBetSlip } from "./MyBetSlip";
+import { MyBetSlip } from "./MyBetSlip";
 import { initialBetValues } from "./betUtils";
 
 export const AddBet = () => {
   const [myBet, setMyBet] = useState<Bet>(initialBetValues);
   const [modifyIndex, setModifyIndex] = useState<number | null>(null);
 
-  // const handleModifyBet = (index: number) => {
-  //   setModifyIndex(index);
-  //   setTimeout(() => {
-  //     window.scrollTo({ top: 0, behavior: "smooth" });
-  //   }, 100);
-  // };
-
-  // TODO: Something that the betbuilder can have multiple selection fields
+  const handleModifyBet = (index: number) => {
+    setModifyIndex(index);
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }, 100);
+  };
 
   console.log("myBet", myBet);
   console.log(
@@ -35,7 +33,7 @@ export const AddBet = () => {
         setModifyIndex={setModifyIndex}
         disabled={myBet.betDetails.length > 0 && modifyIndex === null}
       />
-      {/* {myBet.betDetails.length > 0 && (
+      {myBet.betDetails.length > 0 && (
         <MyBetSlip
           myBet={myBet}
           setMyBet={setMyBet}
@@ -43,7 +41,7 @@ export const AddBet = () => {
           modifyIndex={modifyIndex}
           setModifyIndex={setModifyIndex}
         />
-      )} */}
+      )}
     </div>
   );
 };
