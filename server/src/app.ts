271a7544bd61ca2834eba0f5ebbc8c2ed21d595e
reijjo <teemu.aitomeri@gmail.com>
2025-01-07 19:09:57 +0200

    updated corsOptions

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,10 +5,17 @@ import helmet from "helmet";
 
 const app = express();
 
+const corsOptions = {
+	origin: 'http://localhost:5173',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+	credentials: true,
+	optionsSuccessStatus: 200
+}
+
 app.use(helmet())
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.get("/", (_req, res) => {
   res.send("hello todo");
