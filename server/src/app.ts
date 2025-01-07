d6989b6fd71da2dfd967d91a3fb43fb44577bcb4
reijjo <teemu.aitomeri@gmail.com>
2025-01-07 19:05:50 +0200

    index and app files ok for now

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(helmet())
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,40 @@
 import * as http from "http";
-import { cyanBright, yellowBright } from "colorette";
-
+import { cyanBright, yellowBright, redBright } from "colorette";
 import app from "./app";
 
-const { PORT } = Bun.env;
+const PORT = Bun.env.PORT || 3000;
+let server = http.createServer(app);
 
-const server = http.createServer(app);
-
-const start = async () => {
-  server.listen(PORT, () => {
-    console.log(yellowBright(`ENV = '${Bun.env.NODE_ENV}'`));
-    console.log(cyanBright(`Server on port ${PORT}.`));
+const restartServer = () => {
+  server.close(() => {
+    server = http.createServer(app);
+    startServer();
   });
 };
 
-start();
+// Error handlers
+process.on("uncaughtException", (err) => {
+  console.error(redBright("UNCAUGHT EXCEPTION! Restarting..."));
+  console.error(err);
+  restartServer();
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error(redBright("UNHANDLED REJECTION! Restarting..."));
+  console.error(err);
+  restartServer();
+});
+
+const startServer = async () => {
+  try {
+    server.listen(PORT, () => {
+      console.log(yellowBright(`ENV = '${Bun.env.NODE_ENV}'`));
+      console.log(cyanBright(`Server running on port ${PORT}`));
+    });
+  } catch (error) {
+    console.error(redBright("Error starting server:"), error);
+    setTimeout(startServer, 5000);
+  }
+};
+
+startServer();
