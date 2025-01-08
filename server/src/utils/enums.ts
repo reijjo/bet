c7dcd1e7691f0103d305963263bf72a45208cbe4
reijjo <teemu.aitomeri@gmail.com>
2025-01-08 18:01:26 +0200

    dockercompose coming up

---
+++ server/src/utils/enums.ts
@@ -0,0 +1,79 @@
+export const SportLeague = {
+  NFL: "NFL",
+  NBA: "NBA",
+  MLB: "MLB",
+  NHL: "NHL",
+  MLS: "MLS",
+  Soccer: "Soccer",
+  IceHockey: "Ice Hockey",
+  Basketball: "Basketball",
+  AmFootball: "American Football",
+  Mixed: "Mixed",
+  Other: "Other",
+} as const;
+
+export type SportLeague = typeof SportLeague[keyof typeof SportLeague];
+
+export const BetStatus = {
+  Pending: "Pending",
+  Won: "Won",
+  Lost: "Lost",
+  Void: "Void",
+  HalfWon: "Half Won",
+  HalfLost: "Half Lost",
+} as const;
+
+export type BetStatus = typeof BetStatus[keyof typeof BetStatus];
+
+export const Bookmaker = {
+  EpicBet: "EpicBet",
+  Bet365: "Bet365",
+  Unibet: "Unibet",
+  Veikkaus: "Veikkaus",
+  Other: "Other",
+} as const;
+
+export type Bookmaker = typeof Bookmaker[keyof typeof Bookmaker];
+
+export const BetType = {
+  Single: "Single",
+  Double: "Double",
+  Treble: "Treble",
+  Parlayx4: "Parlay x4",
+  BigParlay: "Parlay x5+",
+  Over: "Over",
+  Under: "Under",
+  BetBuilder: "Bet Builder",
+  Other: "Other",
+  Btts: "Both Teams to Score",
+  Moniveto: "Moniveto",
+  Tulosveto: "Tulosveto",
+  Tuplaus: "Ladder Challenge",
+  Props: "Player Props",
+} as const;
+
+export type BetType = typeof BetType[keyof typeof BetType];
+
+export const LimitedBetType = {
+  Single: "Single",
+  Over: "Over",
+  Under: "Under",
+  BetBuilder: "Bet Builder",
+  Btts: "Both Teams to Score",
+  Moniveto: "Moniveto",
+  Tulosveto: "Tulosveto",
+  Tuplaus: "Ladder Challenge",
+  Props: "Player Props",
+  Other: "Other",
+} as const;
+
+export type LimitedBetType = typeof LimitedBetType[keyof typeof LimitedBetType];
+
+export const BetBuilderInputTypes = {
+  BetBuilder: "Bet Builder",
+  Tuplaus: "Ladder Challenge",
+  Props: "Player Props",
+  Other: "Other",
+} as const;
+
+export type BetBuilderInputTypes = typeof BetBuilderInputTypes[keyof typeof BetBuilderInputTypes];
---
+++ server/src/utils/types.ts
@@ -0,0 +1,29 @@
+import type { Bookmaker, BetStatus, BetType, SportLeague } from "./enums";
+
+export type Bet = {
+  id?: number | string;
+  user_id: number;
+  stake: string | number;
+  bookmaker: Bookmaker;
+  tipper: string;
+  status: BetStatus;
+  bet_final_type: BetType;
+  sport: SportLeague;
+  notes?: string;
+  betDetails: BetDetails[];
+};
+
+export type BetDetails = {
+  date: string;
+  home_team: string;
+  away_team: string;
+  selection: string;
+  odds: number | string;
+  home_result?: string;
+  away_result?: string;
+  betbuilder_selection?: string[];
+  betbuilder_result?: string[];
+  freebet: boolean;
+  livebet: boolean;
+  bet_type: BetType;
+};
