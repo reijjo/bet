088161ec2948b41fcbe41f41cb63b1ca03981a21
reijjo <teemu.aitomeri@gmail.com>
2025-01-27 21:15:13 +0200

    huh database working again

--- server/src/utils/config.ts
+++ server/src/utils/config.ts
@@ -1,37 +1,50 @@
 const envVars = [
-	"PORT",
+  "PORT",
   "DB_NAME",
   "DB_PORT",
+  "DB_LOCAL",
   "DB_HOST",
-	"DB_MAINTENANCE",
+  "DB_MAINTENANCE",
   "POSTGRES_USER",
   "POSTGRES_PASSWORD",
-	"PGADMIN_DEFAULT_EMAIL",
-	"PGADMIN_DEFAULT_PASSWORD",
-	"PG_PORT"
+  "PGADMIN_DEFAULT_EMAIL",
+  "PGADMIN_DEFAULT_PASSWORD",
+  "PG_PORT",
 ] as const;
 
 // Validates that env variables exists
 for (const varName of envVars) {
-	if (!Bun.env[varName]) {
-		throw new Error(`Missing env variable: ${varName}`)
-	}
+  if (!Bun.env[varName]) {
+    throw new Error(`Missing env variable: ${varName}`);
+  }
 }
 
-const { PORT, DB_NAME, DB_PORT, DB_HOST, DB_MAINTENANCE, POSTGRES_USER, POSTGRES_PASSWORD, PGADMIN_DEFAULT_EMAIL, PGADMIN_DEFAULT_PASSWORD, PG_PORT } = Bun.env
-const DATABASE_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
-
+const {
+  PORT,
+  DB_NAME,
+  DB_PORT,
+  DB_LOCAL,
+  DB_HOST,
+  DB_MAINTENANCE,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  PGADMIN_DEFAULT_EMAIL,
+  PGADMIN_DEFAULT_PASSWORD,
+  PG_PORT,
+} = Bun.env;
+const DATABASE_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${DB_LOCAL}:${DB_PORT}/${DB_NAME}`;
 
 export const config = {
-	PORT,
-	DB_NAME,
-	DB_PORT,
-	DB_HOST,
-	DB_MAINTENANCE,
-	POSTGRES_USER,
-	POSTGRES_PASSWORD,
-	PGADMIN_DEFAULT_EMAIL,
-	PGADMIN_DEFAULT_PASSWORD,
-	PG_PORT,
-	DATABASE_URL
-}
+  PORT,
+  DB_NAME,
+  DB_PORT,
+  DB_LOCAL,
+  DB_HOST,
+  DB_MAINTENANCE,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  PGADMIN_DEFAULT_EMAIL,
+  PGADMIN_DEFAULT_PASSWORD,
+  PG_PORT,
+  DATABASE_URL,
+};
