600b02873f09ff5960767d4266ffd722ad00f1b3
reijjo <teemu.aitomeri@gmail.com>
2025-04-19 15:51:24 +0300

    supress console.logs in production

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,13 @@ import { Provider } from "react-redux";
 import App from "./App.tsx";
 import { store } from "./store/store.tsx";
 
+if (process.env.MODE === "production") {
+  console.log = () => {};
+  console.debug = () => {};
+  console.info = () => {};
+  console.warn = () => {};
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
