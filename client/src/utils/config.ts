a1db2173f9d6a3d403a92feb5c5e82d8686a8d8e
reijjo <teemu.aitomeri@gmail.com>
2025-04-20 21:14:06 +0300

    mode stuff

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -2,7 +2,8 @@ const { VITE_BACKEND_URL, VITE_DEPLOYMENT_URL } = import.meta.env;
 
 export const config = {
   BACKEND_URL:
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === "production" ||
+    import.meta.env.MODE === "production"
       ? VITE_DEPLOYMENT_URL
       : VITE_BACKEND_URL,
 };
