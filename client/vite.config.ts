0e68d23e4ad0efd7013943137fa1cbe22f1a9725
Reijjo <teemu.aitomeri@gmail.com>
2025-06-06 13:02:27 +0300

    more _redirects for cloudfare pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
