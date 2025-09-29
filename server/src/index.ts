71562c418ced98fade416fa92bae1a1cdd7aa7e0
Reijjo <teemu.aitomeri@gmail.com>
2025-09-29 20:38:25 +0300

    back to render

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import * as http from "http";
-import * as httsp from "https";
-import * as fs from "fs";
+// import * as httsp from "https";
+// import * as fs from "fs";
 
 import app from "./app";
 import { initializeDatabase } from "./models";
@@ -9,20 +9,20 @@ import { connectToDB } from "./utils/db/db";
 import { cyanBright, yellowBright } from "colorette";
 
 const { PORT } = config;
-let server: http.Server | httsp.Server;
+// let server: http.Server | httsp.Server;
 
-if (
-  Bun.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "production"
-) {
-  const httpsOptions = {
-    key: fs.readFileSync("/etc/pki/tls/private/localhost.key"),
-    cert: fs.readFileSync("/etc/pki/tls/certs/localhost.crt"),
-  };
-  server = httsp.createServer(httpsOptions, app);
-} else {
-  server = http.createServer(app);
-}
+// if (
+//   Bun.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "production"
+// ) {
+//   const httpsOptions = {
+//     key: fs.readFileSync("/etc/pki/tls/private/localhost.key"),
+//     cert: fs.readFileSync("/etc/pki/tls/certs/localhost.crt"),
+//   };
+//   server = httsp.createServer(httpsOptions, app);
+// } else {
+let server = http.createServer(app);
+// }
 
 // const restartServer = () => {
 //   server.close(() => {
@@ -31,21 +31,16 @@ if (
 //   });
 // };
 
-const isDeployedPORT =
-  Bun.env.NODE_ENV === "production" || process.env.NODE_ENV === "production"
-    ? 80
-    : PORT;
-
 export const startServer = async () => {
   try {
     await connectToDB();
     await initializeDatabase();
 
-    server.listen(isDeployedPORT, () => {
+    server.listen(PORT, () => {
       console.log(yellowBright(`ENV = '${Bun.env.NODE_ENV}'`));
       console.log(
         cyanBright(
-          `Server running on port ${isDeployedPORT} ${String.fromCodePoint(0x1f41f)}`
+          `Server running on port ${PORT} ${String.fromCodePoint(0x1f41f)}`
         )
       );
     });
